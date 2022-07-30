a2b19b81a8695bf94e06d4d6c7c02270d11e775c
Ted Glynn <will.glynn@gmail.com>
2022-07-30 15:07:37 -0600

    working on styling intro comp

--- src/components/intro/Intro.js
+++ src/components/intro/Intro.js
@@ -1,7 +1,15 @@
 import React from "react";
+import "./intro.css";
 
 const intro = () => {
-  return <>Intro here</>;
+  return (
+    <>
+      <div className="intro-body">
+        <div className="intro-left">left</div>
+        <div className="intro-right">right</div>
+      </div>
+    </>
+  );
 };
 
 export default intro;
