8b74021474bee62cbc4f7a2e0bf9ffe8adcd00a9
Ted Glynn <will.glynn@gmail.com>
2022-07-30 16:09:07 -0600

    more styling and structure for the intro section of page

--- src/components/intro/Intro.js
+++ src/components/intro/Intro.js
@@ -5,7 +5,21 @@ const intro = () => {
   return (
     <>
       <div className="intro-body">
-        <div className="intro-left">left</div>
+        <div className="intro-left">
+          <div className="intro-left-wrap">
+            <h2 className="greeting">Hi, my name is</h2>
+            <h1 className="intro-name">Ted Glynn</h1>
+            <div className="intro-titles">
+              <div className="intro-titles-wrap">
+                <div className="intro-titles-item">DU Bootcamp Grad</div>
+                <div className="intro-titles-item">Web Developer</div>
+                <div className="intro-titles-item">UX/UI Enthusiast</div>
+                <div className="intro-titles-item">Bartender</div>
+                <div className="intro-titles-item">Good Human</div>
+              </div>
+            </div>
+          </div>
+        </div>
         <div className="intro-right">right</div>
       </div>
     </>
