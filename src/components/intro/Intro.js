4d8897ed065a8bcca8b039aca2f5015309e3baf1
Ted G <61029718+ruxpin86@users.noreply.github.com>
2022-08-02 12:13:46 -0600

    got intro animations moving with scroller

--- src/components/intro/Intro.js
+++ src/components/intro/Intro.js
@@ -1,5 +1,6 @@
 import React from "react";
 import "./intro.css";
+import Me from "../../img/me.jpg";
 
 const intro = () => {
   return (
@@ -7,16 +8,23 @@ const intro = () => {
       <div className="intro-body">
         <div className="intro-left">
           <div className="intro-left-wrap">
-            <h2 className="greeting">Hi, my name is</h2>
+            <h2 className="greeting">Hi! My name is</h2>
             <h1 className="intro-name">Ted Glynn</h1>
             <div className="intro-titles">
               <div className="intro-titles-wrap">
-                <div className="intro-titles-item">DU Bootcamp Grad</div>
+                <div className="intro-titles-item">DU Bootcamp Graduate</div>
                 <div className="intro-titles-item">Web Developer</div>
                 <div className="intro-titles-item">UX/UI Enthusiast</div>
                 <div className="intro-titles-item">Bartender</div>
-                <div className="intro-titles-item">Good Human</div>
+                <div className="intro-titles-item">DIY Guy</div>
+                <div className="intro-titles-item">Problem Solver</div>
               </div>
+            </div>
+            <div className="intro-description">
+              I am a recently minted bootcamp graduate, motivated to begin a new
+              career in the tech industry after spending years in service. I
+              enjoy creating stylish and responsive front ends that feel sleak
+              and fun, as well as push me to learn skills.
             </div>
           </div>
         </div>
