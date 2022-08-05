b592f17c208b9bf52220e73413940c8186253b5d
Ted G <61029718+ruxpin86@users.noreply.github.com>
2022-08-04 20:45:53 -0600

    last changes before calling it a night

--- src/components/about/About.js
+++ src/components/about/About.js
@@ -4,7 +4,10 @@ import "./about.css";
 export default function About() {
   return (
     <div className="about">
-      <div className="about-left"></div>
+      <div className="about-left">
+        <div className="about-card-bg"></div>
+        <div className="about-card"></div>
+      </div>
       <div className="about-right"></div>
     </div>
   );
