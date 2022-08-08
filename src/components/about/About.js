4cfeb14d7bfd995fcdf918abdbf2611344e1f18c
Ted G <61029718+ruxpin86@users.noreply.github.com>
2022-08-07 22:21:22 -0600

    working on the about me section and got second image in and sized properly

--- src/components/about/About.js
+++ src/components/about/About.js
@@ -1,12 +1,15 @@
 import React from "react";
 import "./about.css";
+import Img from "../../img/look-up.png";
 
 export default function About() {
   return (
     <div className="about">
       <div className="about-left">
         <div className="about-card-bg"></div>
-        <div className="about-card"></div>
+        <div className="about-card">
+          <img className="about-img" src={Img} />
+        </div>
       </div>
       <div className="about-right"></div>
     </div>
