cf9f83effd7cc640443e38ecfb1d4e1c73859837
Ted G <61029718+ruxpin86@users.noreply.github.com>
2022-08-09 13:54:36 -0600

    about me is in place, need to move some elements around though

--- src/components/about/About.js
+++ src/components/about/About.js
@@ -11,7 +11,20 @@ export default function About() {
           <img className="about-img" src={Img} />
         </div>
       </div>
-      <div className="about-right"></div>
+      <div className="about-right">
+        <h1 className="about-title">Nice to meet you!</h1>
+        <p className="about-body">
+          At the beginning of August I completed the University of Denver's Full
+          Stack Coding Boot Camp and I am now currently seeking my first
+          position as a full time web developer. As I continue to build
+          projects, I am enjoying getting to focus on the aspects of front end
+          design that make websites feel more dynamic and exciting for users by
+          learning technologies like GSAP and Spline. In my free time I enjoy
+          fly-fishing, golfing, creative writing, and hanging out with my best
+          man Odie. Below you will find some example of the work I have done
+          both in my course as well as on my own!
+        </p>
+      </div>
     </div>
   );
 }
