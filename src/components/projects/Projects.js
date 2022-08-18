678a0383e062cb56022811da29cee919a8530d9f
Ted G <61029718+ruxpin86@users.noreply.github.com>
2022-08-18 12:20:24 -0600

    project card header built to mimic browser window

--- src/components/projects/Projects.js
+++ src/components/projects/Projects.js
@@ -2,5 +2,14 @@ import React from "react";
 import "./projects.css";
 
 export default function Projects() {
-  return <div className="project">Card</div>;
+  return (
+    <div className="project">
+      <div className="project-header">
+        <div className="circle1"></div>
+        <div className="circle2"></div>
+        <div className="circle3"></div>
+      </div>
+      <a href="#" target="_blank"></a>
+    </div>
+  );
 }
