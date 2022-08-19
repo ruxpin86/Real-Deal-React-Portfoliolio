bcfc583a7123cc03f280efde5b8cc70768c8e0da
Ted G <61029718+ruxpin86@users.noreply.github.com>
2022-08-19 17:06:42 -0600

    working on getting styling for contact sorted out

--- src/components/contact/Contact.js
+++ src/components/contact/Contact.js
@@ -9,7 +9,18 @@ export default function Contact() {
         <div className="contact-left">
           <h1 className="contact-title">I'd love to hear from you!</h1>
           <div className="contact-info">
-            <div className="contact-item"></div>
+            <div className="contact-item">
+              <img src="img/iphone-icon.png" alt="" className="contact-icon" />
+              (720) 429-4570
+            </div>
+            <div className="contact-item">
+              <img src="img/gmail-icon.png" alt="" className="contact-icon" />
+              will.glynn@gmail.com
+            </div>
+            <div className="contact-item">
+              <img src="img/location.png" alt="" className="contact-icon" />
+              3069 Tejon St. Denver, CO
+            </div>
           </div>
         </div>
         <div className="contact-right"></div>
