17b3faeef953ba844f581f81d125d59097e8e567
Ted G <61029718+ruxpin86@users.noreply.github.com>
2022-08-19 16:18:49 -0600

    getting project animations to scroll on hover

--- src/components/projectsList/ProjectsList.js
+++ src/components/projectsList/ProjectsList.js
@@ -10,9 +10,9 @@ export default function ProjectsList() {
         <h1 className="project-title">Check out my work to date!</h1>
         <p className="project-desc">
           Here are some examples of projects that I have completed to date. I am
-          currently working on a couple more, so this will be updated regularly
-          over the coming weeks as I prepare for interviews with potential
-          employers like yourselves. I hope you enjoy!
+          continuously working on more, so this will be updated regularly over
+          the coming weeks as I prepare for interviews with potential employers
+          like yourselves. I hope you enjoy!
         </p>
       </div>
       <div className="project-ul">
