7ea4b4bf01932e7aeb0d54aeafe34fea6bb934e4
Ted G <61029718+ruxpin86@users.noreply.github.com>
2022-08-21 21:11:28 -0600

    importing new theme component to app

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import Intro from "./components/intro/Intro.js";
 import About from "./components/about/About";
 import ProjectsList from "./components/projectsList/ProjectsList.js";
 import Contact from "./components/contact/Contact.js";
+import Theme from "./components/toggle/Theme.js";
 import "./styles/globals.css";
 
 const App = () => {
   return (
     <div>
+      <Theme />
       <Intro />
       <About />
       <ProjectsList />
---
+++ src/components/toggle/Theme.js
@@ -0,0 +1,6 @@
+import React from "react";
+import "./theme.css";
+
+export default function Theme() {
+  return <div className="theme">Theme</div>;
+}
