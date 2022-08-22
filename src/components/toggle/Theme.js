421dec736a15b9cdec49c6ccd1b2c68db9178f81
Ted G <61029718+ruxpin86@users.noreply.github.com>
2022-08-21 21:21:03 -0600

    importing theme slider images

--- src/components/toggle/Theme.js
+++ src/components/toggle/Theme.js
@@ -2,5 +2,11 @@ import React from "react";
 import "./theme.css";
 
 export default function Theme() {
-  return <div className="theme">Theme</div>;
+  return (
+    <div className="theme">
+      <img src="#" alt="" className="theme-icon" />
+      <img src="#" alt="" className="theme-icon" />
+      <div className="theme-button"></div>
+    </div>
+  );
 }
