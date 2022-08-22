fc7fddb03d51bdc3198bb2ac7798286015046385
Ted G <61029718+ruxpin86@users.noreply.github.com>
2022-08-22 16:53:06 -0600

    styling for my theme toggle is in place

--- src/components/toggle/Theme.js
+++ src/components/toggle/Theme.js
@@ -4,8 +4,8 @@ import "./theme.css";
 export default function Theme() {
   return (
     <div className="theme">
-      <img src="#" alt="" className="theme-icon" />
-      <img src="#" alt="" className="theme-icon" />
+      <img src="img/sun.png" alt="" className="theme-icon" />
+      <img src="img/moon.png" alt="" className="theme-icon" />
       <div className="theme-button"></div>
     </div>
   );
